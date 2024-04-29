c2382c618bca3fd8f2a9deac80ee1911a8324d3f
YumiCouffin <116382827+YumiCouffin@users.noreply.github.com>
2024-04-29 12:57:49 +0200

    Revert "j'ai essayé de fix"
    This reverts commit 2bef89f21f8d1b7dd09d7d3f5daec1246a112587.

--- Assets/=Parapluie/Scripts/SceneManager.cs
+++ Assets/=Parapluie/Scripts/SceneManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
